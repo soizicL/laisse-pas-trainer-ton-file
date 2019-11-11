00a98bea90c36d1c2611406ee459d3e6b17c0d25
soizic Lambert <soizic.lee@gmail.com>
2019-11-11 20:52:14 +0100

    final commit

--- upload.php
+++ upload.php
@@ -61,7 +61,7 @@ if(!empty($_FILES['upload_file'])) {
     <title>UPLOAD DE FICHIER</title>
     <!----------------------import bootstrap--------------------------->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <!----------------------Link style CSS--------------------------->
+
 </head>
 
 <body>
